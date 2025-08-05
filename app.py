17599a151f0f142bac1d935aeee111c30698b6f4
ahmed-fortest <fortestqewr@gmail.com>
2025-08-05 14:37:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def predict():
         return render_template('index.html', result=error)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=False))
